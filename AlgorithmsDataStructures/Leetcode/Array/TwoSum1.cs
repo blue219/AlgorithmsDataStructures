b94e6a63142ec0daabba58ed4a4eecd539066c62
sky <1255067427@qq.com>
2025-08-05 01:06:01 +1200

    Array: TwoSum1

---
+++ AlgorithmsDataStructures/Leetcode/Array/TwoSum1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructures.Leetcode.Array
+{
+    internal class TwoSum1
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3, 2, 4 };
+            int target = 6;
+            TwoSum(nums,target);
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    int sum = nums[i] + nums[j];
+                    if (sum == target)
+                    {
+                        int[] resultArray = { i, j };
+                        return resultArray;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
